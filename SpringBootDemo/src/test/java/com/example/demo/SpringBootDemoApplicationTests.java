bb1c734e425976a6277713ea95b67d878126d105
hsham <hhs9102@naver.com>
2019-04-19 22:54:17 +0900

    mock status Test

--- SpringBootDemo/src/test/java/com/example/demo/SpringBootDemoApplicationTests.java
+++ SpringBootDemo/src/test/java/com/example/demo/SpringBootDemoApplicationTests.java
@@ -2,13 +2,20 @@ package com.example.demo;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import junit.framework.Assert;
 
@@ -16,7 +23,17 @@ import junit.framework.Assert;
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
 
+	private MockMvc mockMvc;
+	
 	List<String> mockedList = mock(List.class);
+	
+	@Autowired
+	DemoController demoController;
+	
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(demoController).build();
+	}
 	
 	@Test
 	public void contextLoads() {
@@ -28,7 +45,6 @@ public class SpringBootDemoApplicationTests {
 		mockedList.add("test");
 		mockedList.add("wow~~^^");
 		System.out.println(mockedList);
-		
 		Assert.assertEquals(0, mockedList.size());	//mock된 것은 실제 객체에 영향을 끼치지 않는다.
 	}
 	
@@ -38,6 +54,20 @@ public class SpringBootDemoApplicationTests {
 		mockedList.add("wow~~^^");
 		System.out.println(mockedList);
 		verify(mockedList).add("test");
+	}
+	
+	@Test
+	public void requsetTest() throws Exception {
+		this.mockMvc.perform(get("/"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void failTest() throws Exception {
+		this.mockMvc.perform(get("notExists"))
+			.andDo(print())
+			.andExpect(status().isOk());
 	}
 
 }
